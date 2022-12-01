22f36d1f7502f1af591d8677a63b11d0870581ea
Jeniffer Carmo <72942750+Carmosz@users.noreply.github.com>
2022-11-30 22:35:46 -0300

    Add files via upload

---
+++ Hora-de-codar-2-Java/Exercicio4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in); 
+       
+       int total = 0;
+       
+       for (int i = 0; i < 10; i++) {
+
+           System.out.println("Digite um valor: ");
+           int n1 = scan.nextInt();
+           total += n1;
+       }
+
+       System.out.println("A média artimética desses 10 valores foi de: " + (total / 10));
+           
+}
+}
